8f2e6a70c15efd3b78648b8719e48895b31a6ecc
sallythetechie <sllmusanga7@gmail.com>
2021-08-20 23:11:43 +0300

    Create bio program

---
+++ task1.js
@@ -0,0 +1,9 @@
+/* 1. Write a program that logs your bio data to the console.
+* note: each bio data should be stored in it's own variable and use naming conventions */
+
+firstName = "Sally";
+lastName = "Musanga"
+email = " sllmusanga7@gmailcom"
+phone = "+254719235608"
+favoriteQuote = "If all else fails...reboot" 
+console.log('My name : ' + ' ' + firstName + ' '+ lastName +  ' '+ '\n' +'phone: ' + ' '  + phone + '\n' +'Email: ' + ' '  + email  + '\n' +'Favorite quote: ' + ' ' + favoriteQuote );
